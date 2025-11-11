7902f20327d60e1c3afe40b3bee17f40f6f1d67f
profitworx <john@profitworx.com>
2025-11-11 20:01:33 +0200

    favicon set

---
+++ src/app/head.tsx
@@ -0,0 +1,13 @@
+export default function Head() {
+  return (
+    <>
+      <link
+        rel="icon"
+        href="/john_deacon_favicon_2025.png"
+        type="image/png"
+        sizes="any"
+      />
+    </>
+  );
+}
+
